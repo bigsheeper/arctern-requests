64a790e2fa23e1fe4b5598d744ce29ce97567694
sheep <954206947@qq.com>
2020-04-02 17:37:54 +0800

    add simple test and pressure test

---
+++ req.py
@@ -0,0 +1,38 @@
+import requests
+import json
+
+def save_png(hex_str, file_name):
+    import base64
+    binary_string = base64.b64decode(hex_str)
+    with open(file_name, 'wb') as png:
+        png.write(binary_string)
+
+def post_request(name, url, save):
+    with open('json/' + name + '.json') as f:
+        data = json.load(f)
+    r = requests.post(url=url, json=data)
+
+    if save:
+        hex_str = r.json()['data']['result']
+        save_png(hex_str, '/tmp/' + name + '.png')
+    return r
+
+def run_simple_test(url):
+    post_request('point_map', url, True)
+    # post_request('weighted_point_map', url, True)
+    post_request('heat_map', url, True)
+    post_request('choropleth_map', url, True)
+    post_request('sql', url, False)
+
+def run_pressure_test(url):
+    import time
+    for i in range(12 * 60):
+        start_time = time.time()
+        post_request('point_map', url, False)
+        print("--- %s seconds ---" % (time.time() - start_time))
+        time.sleep(60)
+
+if __name__ == "__main__":
+    url = 'http://192.168.2.26:8080/db/query'
+    # run_simple_test(url=url)
+    run_pressure_test(url=url)
