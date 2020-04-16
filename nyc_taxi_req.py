85fd78c883c6263ba5a35204b1e5e188c0a04712
sheep <954206947@qq.com>
2020-04-16 16:13:27 +0800

    add icon test

--- nyc_taxi_req.py
+++ nyc_taxi_req.py
@@ -41,10 +41,15 @@ def get_db_id(url_prefix, headers):
     id = str(r.json()['data'][0]['id'])
     return id
 
-def run_sql(url_prefix, headers, json):
+def run_sql(url_prefix, save, headers, json):
     url = url_prefix + '/db/query'
     r = requests.post(url=url, json=json, headers=headers)
     print(r.text)
+
+    if save:
+        hex_str = r.json()['data']['result']
+        save_png(hex_str, '/tmp/offscreen.png')
+    return r
 
 if __name__ == "__main__":
     # sheep:
@@ -57,10 +62,11 @@ if __name__ == "__main__":
     id = get_db_id(url_prefix=url_prefix, headers=headers)
     get_db_info(url_prefix=url_prefix, id=id, headers=headers)
 
-    run_sql(url_prefix=url_prefix, headers=headers, json={'id': id, 'query': {'type': 'sql', 'sql': 'select count(*) from nyc_taxi'}})
-    run_sql(url_prefix=url_prefix, headers=headers, json={'id': id, 'query': {'sql': 'SELECT count(*) AS countval FROM nyc_taxi', 'type': 'sql'}})
-    run_sql(url_prefix=url_prefix, headers=headers, json={'id': id, 'query': {'type': 'sql', 'sql': 'SELECT MIN(fare_amount) as min , MAX(fare_amount) as max FROM nyc_taxi'}})
-    run_sql(url_prefix=url_prefix, headers=headers, json={'id': id, 'query': {'sql': "SELECT ST_Point (dropoff_longitude, dropoff_latitude) AS point, avg(fare_amount) AS w FROM nyc_taxi WHERE (ST_Within (ST_Point (dropoff_longitude, dropoff_latitude), ST_GeomFromText('POLYGON ((-73.5 40.1, -73.5 41.1, -70.5 41.1, -70.5 40.1, -73.5 40.1))'))) GROUP BY point", 'type': 'heat', 'params': {'width': 810, 'height': 465, 'heat': {'bounding_box': [-73.5, 40.1, -70.5, 41.1], 'coordinate_system': 'EPSG:4326', 'map_zoom_level': 4, 'aggregation_type': 'avg'}}}})
-    run_sql(url_prefix=url_prefix, headers=headers, json={'id': id, 'query': {'type': 'sql', 'sql': 'SELECT dropoff_longitude, dropoff_latitude FROM nyc_taxi WHERE dropoff_longitude IS NOT NULL AND dropoff_latitude IS NOT NULL LIMIT 1'}})
-    run_sql(url_prefix=url_prefix, headers=headers, json={'id': id, 'query': {'sql': "SELECT ST_Point (dropoff_longitude, dropoff_latitude) AS point, avg(fare_amount) AS w FROM nyc_taxi WHERE (ST_Within (ST_Point (dropoff_longitude, dropoff_latitude), ST_GeomFromText('POLYGON ((-82.73103190893681 29.40507162320695, -82.73103190893681 50.36222793457449, -65.15290690893714 50.36222793457449, -65.15290690893714 29.40507162320695, -82.73103190893681 29.40507162320695))'))) GROUP BY point", 'type': 'heat', 'params': {'width': 400, 'height': 630, 'heat': {'bounding_box': [-82.73103190893681, 29.40507162320695, -65.15290690893714, 50.36222793457449], 'coordinate_system': 'EPSG:4326', 'map_zoom_level': 4, 'aggregation_type': 'avg'}}}})
-    # run_sql(url_prefix=url_prefix, headers=headers, json={'id': id, 'query': {'sql': "SELECT ST_Point (dropoff_longitude, dropoff_latitude) AS point, avg(fare_amount) AS w FROM nyc_taxi WHERE (ST_Within (ST_Point (dropoff_longitude, dropoff_latitude), ST_GeomFromText('POLYGON ((-179.89999999999978 -26.400509849772284, -179.89999999999978 81.74354709384872, -66.8854430000034 81.74354709384872, -66.8854430000034 -26.400509849772284, -179.89999999999978 -26.400509849772284))'))) GROUP BY point", 'type': 'heat', 'params': {'width': 400, 'height': 630, 'heat': {'bounding_box': [-179.89999999999978, -26.400509849772284, -66.8854430000034, 81.74354709384872], 'coordinate_system': 'EPSG:4326', 'map_zoom_level': 1.3153444833432348, 'aggregation_type': 'avg'}}}})
+    run_sql(url_prefix=url_prefix, save=False, headers=headers, json={'id': id, 'query': {'type': 'sql', 'sql': 'select count(*) from nyc_taxi'}})
+    run_sql(url_prefix=url_prefix, save=False, headers=headers, json={'id': id, 'query': {'sql': 'SELECT count(*) AS countval FROM nyc_taxi', 'type': 'sql'}})
+    run_sql(url_prefix=url_prefix, save=False, headers=headers, json={'id': id, 'query': {'type': 'sql', 'sql': 'SELECT MIN(fare_amount) as min , MAX(fare_amount) as max FROM nyc_taxi'}})
+    run_sql(url_prefix=url_prefix, save=False, headers=headers, json={'id': id, 'query': {'sql': "SELECT ST_Point (dropoff_longitude, dropoff_latitude) AS point, avg(fare_amount) AS w FROM nyc_taxi WHERE (ST_Within (ST_Point (dropoff_longitude, dropoff_latitude), ST_GeomFromText('POLYGON ((-73.5 40.1, -73.5 41.1, -70.5 41.1, -70.5 40.1, -73.5 40.1))'))) GROUP BY point", 'type': 'heat', 'params': {'width': 810, 'height': 465, 'heat': {'bounding_box': [-73.5, 40.1, -70.5, 41.1], 'coordinate_system': 'EPSG:4326', 'map_zoom_level': 4, 'aggregation_type': 'avg'}}}})
+    run_sql(url_prefix=url_prefix, save=False, headers=headers, json={'id': id, 'query': {'sql': "SELECT ST_Point (dropoff_longitude, dropoff_latitude) AS point FROM nyc_taxi WHERE (ST_Within (ST_Point (dropoff_longitude, dropoff_latitude), ST_GeomFromText('POLYGON ((-73.5 40.1, -73.5 41.1, -70.5 41.1, -70.5 40.1, -73.5 40.1))'))) GROUP BY point", 'type': 'icon', 'params': {'width': 810, 'height': 465, 'icon': {'bounding_box': [-73.5, 40.1, -70.5, 41.1], 'coordinate_system': 'EPSG:4326', 'icon_path': '/tmp/taxi.png'}}}})
+    run_sql(url_prefix=url_prefix, save=False, headers=headers, json={'id': id, 'query': {'type': 'sql', 'sql': 'SELECT dropoff_longitude, dropoff_latitude FROM nyc_taxi WHERE dropoff_longitude IS NOT NULL AND dropoff_latitude IS NOT NULL LIMIT 1'}})
+    run_sql(url_prefix=url_prefix, save=False, headers=headers, json={'id': id, 'query': {'sql': "SELECT ST_Point (dropoff_longitude, dropoff_latitude) AS point, avg(fare_amount) AS w FROM nyc_taxi WHERE (ST_Within (ST_Point (dropoff_longitude, dropoff_latitude), ST_GeomFromText('POLYGON ((-82.73103190893681 29.40507162320695, -82.73103190893681 50.36222793457449, -65.15290690893714 50.36222793457449, -65.15290690893714 29.40507162320695, -82.73103190893681 29.40507162320695))'))) GROUP BY point", 'type': 'heat', 'params': {'width': 400, 'height': 630, 'heat': {'bounding_box': [-82.73103190893681, 29.40507162320695, -65.15290690893714, 50.36222793457449], 'coordinate_system': 'EPSG:4326', 'map_zoom_level': 4, 'aggregation_type': 'avg'}}}})
+    run_sql(url_prefix=url_prefix, save=False, headers=headers, json={'id': id, 'query': {'sql': "SELECT ST_Point (dropoff_longitude, dropoff_latitude) AS point, avg(fare_amount) AS w FROM nyc_taxi WHERE (ST_Within (ST_Point (dropoff_longitude, dropoff_latitude), ST_GeomFromText('POLYGON ((-179.89999999999978 -26.400509849772284, -179.89999999999978 81.74354709384872, -66.8854430000034 81.74354709384872, -66.8854430000034 -26.400509849772284, -179.89999999999978 -26.400509849772284))'))) GROUP BY point", 'type': 'heat', 'params': {'width': 400, 'height': 630, 'heat': {'bounding_box': [-179.89999999999978, -26.400509849772284, -66.8854430000034, 81.74354709384872], 'coordinate_system': 'EPSG:4326', 'map_zoom_level': 1.3153444833432348, 'aggregation_type': 'avg'}}}})
